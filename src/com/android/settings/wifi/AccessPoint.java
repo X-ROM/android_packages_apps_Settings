a706fd458da5047464f28f18cc482ab0912952f7
Bruno Randolf <br1@einfach.org>
2013-02-02 11:10:13 +0000

    Wifi: Allow configuration of Ad-Hoc (IBSS) networks
    Show Ad-Hoc (IBSS) networks in the list of available Wi-Fi networks and allow
    to connect to or create new Ad-Hoc (IBSS) networks.
    
    This requires the IBSS changes in frameworks/base and external/wpa_supplicant_8
    
    Change-Id: Ie44c47753100bbf8e691446e69b8d31659f64e3b
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/settings/wifi/AccessPoint.java

--- src/com/android/settings/wifi/AccessPoint.java
+++ src/com/android/settings/wifi/AccessPoint.java
@@ -63,6 +63,8 @@ class AccessPoint extends Preference {
     int security;
     int networkId;
     boolean wpsAvailable = false;
+    boolean isIBSS = false;
+    int frequency;
 
     PskType pskType = PskType.UNKNOWN;
 
@@ -196,6 +198,8 @@ class AccessPoint extends Preference {
         security = getSecurity(config);
         networkId = config.networkId;
         mRssi = Integer.MAX_VALUE;
+        isIBSS = config.isIBSS;
+        frequency = config.frequency;
         mConfig = config;
     }
 
@@ -204,6 +208,8 @@ class AccessPoint extends Preference {
         bssid = result.BSSID;
         security = getSecurity(result);
         wpsAvailable = security != SECURITY_EAP && result.capabilities.contains("WPS");
+        isIBSS = result.capabilities.contains("[IBSS]");
+        frequency = result.frequency;
         if (security == SECURITY_PSK)
             pskType = getPskType(result);
         networkId = -1;
@@ -346,24 +352,29 @@ class AccessPoint extends Preference {
         setTitle(ssid);
 
         Context context = getContext();
+
+        StringBuilder summary = new StringBuilder();
+
+        if (isIBSS)
+            summary.append(context.getString(R.string.wifi_mode_ibss_short)).append(" ");
+
         if (mConfig != null && mConfig.status == WifiConfiguration.Status.DISABLED) {
             switch (mConfig.disableReason) {
                 case WifiConfiguration.DISABLED_AUTH_FAILURE:
-                    setSummary(context.getString(R.string.wifi_disabled_password_failure));
+                    summary.append(context.getString(R.string.wifi_disabled_password_failure));
                     break;
                 case WifiConfiguration.DISABLED_DHCP_FAILURE:
                 case WifiConfiguration.DISABLED_DNS_FAILURE:
-                    setSummary(context.getString(R.string.wifi_disabled_network_failure));
+                    summary.append(context.getString(R.string.wifi_disabled_network_failure));
                     break;
                 case WifiConfiguration.DISABLED_UNKNOWN_REASON:
-                    setSummary(context.getString(R.string.wifi_disabled_generic));
+                    summary.append(context.getString(R.string.wifi_disabled_generic));
             }
         } else if (mRssi == Integer.MAX_VALUE) { // Wifi out of range
             setSummary(context.getString(R.string.wifi_not_in_range));
         } else if (mState != null) { // This is the active connection
             setSummary(Summary.get(context, mState));
         } else { // In range, not disabled.
-            StringBuilder summary = new StringBuilder();
             if (mConfig != null) { // Is saved network
                 summary.append(context.getString(R.string.wifi_remembered));
             }
@@ -385,8 +396,8 @@ class AccessPoint extends Preference {
                     summary.append(context.getString(R.string.wifi_wps_available_second_item));
                 }
             }
-            setSummary(summary.toString());
         }
+        setSummary(summary.toString());
     }
 
     /**
