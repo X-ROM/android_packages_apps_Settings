spezi77/android_packages_apps_Settings-2

Settings: Add new configuration qualifier for UI change on the fly (3/3)
last night I got an idea after 5 weeks of breaking stuff and
doing to much complicated things. Together with mnazim we tried
today to realize this idea. So another absolut new unique SlimBean
feature is born.

The aim we wanted at the end was to give a simple and resource friendly
way to change the complete UI of a rom on the fly. So why not use
the great fallback mechanism of AOSP and change/add a new
qualifier into the AOSP core. To explain how it works.....if you want
to add a new UI just add in /res folder of any app or framework you want
folders with the qualifier "inverted". Eg. drawable-inverted-xhdpi
or values-inverted or layout-port-inverted or whatever you want. Put in
only the files you want to modify thats it.

All qualifier in AOSP are following strict rules. You can read here.
Do not forget the order of qualifier is important. The new "inverted"
qualifier we added to the UIMode qualifier. To get more information
about read here

http://developer.android.com/guide/topics/resources/providing-resources.html

So what this commit does:
- add new qualifier "inverted"
- support for API level 13 and higher
- change the custom UI on the fly
- control default UI Mode over config.xml
- working for all apps. They just need to include folders
  with the new qualifier name
- resource friendly....no resource wasting either programmticaly
  then source blow, because you just need to put in the files you
  want to change
- cause of the qualifier structure compatible with every phone,
  language or whatever qualifier you want to use on top
- 100% compatible to theme manager. If a theme misses drawables
  they are replaced with them from the current UIMode which is set.

What is the benefit why not use just a theme? We started half year
ago TRDS which was inverted gapps system base themed....means we
morphed directly the changes into the framework. This methoed was
on of the reasons why trds got so 

src/com/android/settings/cyanogenmod/TRDSEnabler.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2013 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.util.Slog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
+    private final Context mContext;
+    private Switch mSwitch;
+    private boolean mStateMachineEvent;
+
+    public TRDSEnabler(Context context, Switch switch_) {
+        mContext = context;
+        mSwitch = switch_;
+    }
+
+    public void resume() {
+        mSwitch.setOnCheckedChangeListener(this);
+        setSwitchState();
+    }
+
+    public void pause() {
+        mSwitch.setOnCheckedChangeListener(null);
+    }
+
+    public void setSwitch(Switch switch_) {
+        if (mSwitch == switch_) return;
+        mSwitch.setOnCheckedChangeListener(null);
+        mSwitch = switch_;
+        mSwitch.setOnCheckedChangeListener(this);
+        setSwitchState();
+    }
+
+    private void setSwitchState() {
+        boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.UI_INVERTED_MODE, 0) == 1;
+        mStateMachineEvent = true;
+        mSwitch.setChecked(enabled);
+        mStateMachineEvent = false;
+    }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (mStateMachineEvent) {
+            return;
+        }
+        // Handle a switch change
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.UI_INVERTED_MODE, isChecked ? 1 : 0);
+    }
+
+}

X-ROM/android_packages_apps_Settings

	res/xml/settings_headers.xml

src/com/android/settings/crom/TRDSEnabler.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2013 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.crom;
+
+import android.util.Slog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
+    private final Context mContext;
+    private Switch mSwitch;
+    private boolean mStateMachineEvent;
+
+    public TRDSEnabler(Context context, Switch switch_) {
+        mContext = context;
+        mSwitch = switch_;
+    }
+
+    public void resume() {
+        mSwitch.setOnCheckedChangeListener(this);
+        setSwitchState();
+    }
+
+    public void pause() {
+        mSwitch.setOnCheckedChangeListener(null);
+    }
+
+    public void setSwitch(Switch switch_) {
+        if (mSwitch == switch_) return;
+        mSwitch.setOnCheckedChangeListener(null);
+        mSwitch = switch_;
+        mSwitch.setOnCheckedChangeListener(this);
+        setSwitchState();
+    }
+
+    private void setSwitchState() {
+        boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.UI_INVERTED_MODE, 0) == 1;
+        mStateMachineEvent = true;
+        mSwitch.setChecked(enabled);
+        mStateMachineEvent = false;
+    }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (mStateMachineEvent) {
+            return;
+        }
+        // Handle a switch change
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.UI_INVERTED_MODE, isChecked ? 1 : 0);
+    }
+
+}