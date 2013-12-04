aa51d12f8771e6dfd896dc45056b4cd1fa1d8673
cristianomatos <cristianobmatos@gmail.com>
2013-12-04 09:57:57 -0200

    RAM bar (2/2)
    Thanks to @kufikugel for hole code ported from 4.3
    
    Change-Id: Icc2307901a9052c647e52c3d3cb6f00805e88aba

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1162,4 +1162,5 @@ public class Settings extends PreferenceActivity
     public static class ApnSettingsActivity extends Settings { /* empty */ }
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class ASSRamBarActivity extends Settings { /* empty */ }
 }
