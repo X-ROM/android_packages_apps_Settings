1a5b1ac6bf667c453913d45968e01e8cadef24ba
aways <aways@c-rom.org>
2014-02-16 04:19:16 +0100

    Test-3 Signed-off-by: aways <aways@c-rom.org>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1163,4 +1163,5 @@ public class Settings extends PreferenceActivity
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
     public static class ASSRamBarActivity extends Settings { /* empty */ }
+    public static class SoundPackageSettingsActivity extends Settings { /* empty */ }
 }
