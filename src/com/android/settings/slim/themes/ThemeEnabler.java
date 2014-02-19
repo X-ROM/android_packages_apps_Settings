thefutureosp/android_packages_apps_Settings-2

settings: TRDS some tweaks and fixes (2/2)
some smaller issues fixed and optimizations:

- fix twilightmode was wrong assigned to wrong value
- ligtsensor mode after first immediattely switch it does
  not allow for 5sec to switch again....after the 5sec it switches
  to current state if the sensor changed its state in this time.
  This prevends on the one hand on bad light conditions a permanent
  switch between dark/light as well it does not bring the system into
  troubles due of to much configuration changes at the same time
- get rid completly of the observe for the UI_THEME_MODE value and
  handle all over the UIModeManager
- adapt slimActions and settings to this new behaviour/method
- some cleanup and minor fixes

Change-Id: I099eb70f179149bcc519a209b93063529349f1e2

src/com/android/settings/crdroid/themes/ThemeEnabler.java
@@ -18,13 +18,11 @@ package com.android.settings.crdroid.themes;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.database.ContentObserver;
-import android.os.Handler;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.Toast;
+
+import com.android.internal.util.crdroid.TRDSConstant;
+import com.android.internal.util.crdroid.TRDSActions;
 
 import com.android.settings.R;
 
@@ -32,50 +30,19 @@ public class ThemeEnabler implements CompoundButton.OnCheckedChangeListener {
     private final Context mContext;
     private Switch mSwitch;
     private boolean mStateMachineEvent;
-    private boolean mEnabled;
-
-    private boolean mAttached;
-    private SettingsObserver mSettingsObserver;
-
-    private final class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            mContext.getContentResolver().registerContentObserver(
-                    Settings.Secure.getUriFor(
-                    Settings.Secure.UI_THEME_AUTO_MODE),
-                    false, this);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            setSwitchState();
-        }
-    }
 
     public ThemeEnabler(Context context, Switch switch_) {
         mContext = context;
         mSwitch = switch_;
-        mSettingsObserver = new SettingsObserver(new Handler());
     }
 
     public void resume() {
         mSwitch.setOnCheckedChangeListener(this);
-        if (!mAttached) {
-            mAttached = true;
-            mSettingsObserver.observe();
-        }
         setSwitchState();
     }
 
     public void pause() {
         mSwitch.setOnCheckedChangeListener(null);
-        if (mAttached) {
-            mAttached = false;
-            mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
-        }
     }
 
     public void setSwitch(Switch switch_) {
@@ -87,10 +54,6 @@ public class ThemeEnabler implements CompoundButton.OnCheckedChangeListener {
     }
 
     public void setSwitchState() {
-        mEnabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.UI_THEME_AUTO_MODE, 0,
-                UserHandle.USER_CURRENT) != 1;
-
         boolean state = mContext.getResources().getConfiguration().uiThemeMode
                     == Configuration.UI_THEME_MODE_HOLO_DARK;
         mStateMachineEvent = true;
@@ -102,21 +65,8 @@ public class ThemeEnabler implements CompoundButton.OnCheckedChangeListener {
         if (mStateMachineEvent) {
             return;
         }
-        if (!mEnabled) {
-            Toast.makeText(mContext, R.string.theme_auto_switch_mode_error,
-                    Toast.LENGTH_SHORT).show();
-            setSwitchState();
-            return;
-        }
-        // Handle a switch change
-        // we currently switch between holodark and hololight till either
-        // theme engine is ready or lightheme is ready. Currently due of
-        // missing light themeing hololight = system base theme
-        Settings.Secure.putIntForUser(mContext.getContentResolver(),
-                Settings.Secure.UI_THEME_MODE, isChecked
-                    ? Configuration.UI_THEME_MODE_HOLO_DARK
-                    : Configuration.UI_THEME_MODE_HOLO_LIGHT,
-                UserHandle.USER_CURRENT);
+        TRDSActions.processAction(mContext, TRDSConstant.ACTION_THEME_SWITCH, false);
+        setSwitchState();
     }
 
 }

X-ROM/android_packages_apps_Settings

src/com/android/settings/slim/themes/ThemeEnabler.java
@@ -18,13 +18,11 @@ package com.android.settings.slim.themes;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.database.ContentObserver;
-import android.os.Handler;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.Toast;
+
+import com.android.internal.util.slim.TRDSConstant;
+import com.android.internal.util.slim.TRDSActions;
 
 import com.android.settings.R;
 
@@ -32,50 +30,19 @@ public class ThemeEnabler implements CompoundButton.OnCheckedChangeListener {
     private final Context mContext;
     private Switch mSwitch;
     private boolean mStateMachineEvent;
-    private boolean mEnabled;
-
-    private boolean mAttached;
-    private SettingsObserver mSettingsObserver;
-
-    private final class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            mContext.getContentResolver().registerContentObserver(
-                    Settings.Secure.getUriFor(
-                    Settings.Secure.UI_THEME_AUTO_MODE),
-                    false, this);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            setSwitchState();
-        }
-    }
 
     public ThemeEnabler(Context context, Switch switch_) {
         mContext = context;
         mSwitch = switch_;
-        mSettingsObserver = new SettingsObserver(new Handler());
     }
 
     public void resume() {
         mSwitch.setOnCheckedChangeListener(this);
-        if (!mAttached) {
-            mAttached = true;
-            mSettingsObserver.observe();
-        }
         setSwitchState();
     }
 
     public void pause() {
         mSwitch.setOnCheckedChangeListener(null);
-        if (mAttached) {
-            mAttached = false;
-            mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
-        }
     }
 
     public void setSwitch(Switch switch_) {
@@ -87,10 +54,6 @@ public class ThemeEnabler implements CompoundButton.OnCheckedChangeListener {
     }
 
     public void setSwitchState() {
-        mEnabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.UI_THEME_AUTO_MODE, 0,
-                UserHandle.USER_CURRENT) != 1;
-
         boolean state = mContext.getResources().getConfiguration().uiThemeMode
                     == Configuration.UI_THEME_MODE_HOLO_DARK;
         mStateMachineEvent = true;
@@ -102,21 +65,8 @@ public class ThemeEnabler implements CompoundButton.OnCheckedChangeListener {
         if (mStateMachineEvent) {
             return;
         }
-        if (!mEnabled) {
-            Toast.makeText(mContext, R.string.theme_auto_switch_mode_error,
-                    Toast.LENGTH_SHORT).show();
-            setSwitchState();
-            return;
-        }
-        // Handle a switch change
-        // we currently switch between holodark and hololight till either
-        // theme engine is ready or lightheme is ready. Currently due of
-        // missing light themeing hololight = system base theme
-        Settings.Secure.putIntForUser(mContext.getContentResolver(),
-                Settings.Secure.UI_THEME_MODE, isChecked
-                    ? Configuration.UI_THEME_MODE_HOLO_DARK
-                    : Configuration.UI_THEME_MODE_HOLO_LIGHT,
-                UserHandle.USER_CURRENT);
+        TRDSActions.processAction(mContext, TRDSConstant.ACTION_THEME_SWITCH, false);
+        setSwitchState();
     }
 
 }