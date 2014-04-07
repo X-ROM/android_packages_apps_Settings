7132d1095650a4150e9890085b5a8c01a5f62b4c
aways <aways@c-rom.org>
2014-04-07 10:04:11 +0200

    Revert "Revert "fix build""
    This reverts commit bb0a936cf542deca5667f6cc2fbe66d70906a2ab.

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -440,7 +440,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
         // WhisperPush
         // Only add if device has telephony support and has WhisperPush installed.
-+        if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
+        if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
                 && isPackageInstalled("org.whispersystems.whisperpush")) {
             addPreferencesFromResource(R.xml.security_settings_whisperpush);
         }
