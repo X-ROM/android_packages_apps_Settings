81ded4df5baa6d53078e1e2d030cf07f639c0cf7
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-04-07 14:26:30 +0200

    [2/2] Settings: Implement App circle sidebar

--- src/com/android/settings/crom/MiscOptions.java
+++ src/com/android/settings/crom/MiscOptions.java
@@ -17,33 +17,66 @@
 
 package com.android.settings.crom;
 
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ComponentName;
 import android.app.admin.DeviceAdminReceiver;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.text.Editable;
+import android.util.Slog;
+import android.view.IWindowManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.preference.SeekBarPreference;
 import android.provider.Settings;
+import android.text.TextUtils;
 
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import com.android.settings.hfm.HfmHelpers;
+import com.android.settings.crom.tools.AppMultiSelectListPreference;
+import com.android.internal.util.slim.DeviceUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.lang.Thread;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MiscOptions extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
     private static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+    private static final String PREF_INCLUDE_APP_CIRCLE_BAR_KEY = "app_circle_bar_included_apps";
 
+    private AppMultiSelectListPreference mIncludedAppCircleBar;
     private CheckBoxPreference mDisableFC;
     private CheckBoxPreference mHfmDisableAds;
 
@@ -53,6 +86,7 @@ public class MiscOptions extends SettingsPreferenceFragment implements OnPrefere
 
         addPreferencesFromResource(R.xml.misc_options);
 
+        PreferenceScreen prefSet = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
 
         mDisableFC = (CheckBoxPreference) findPreference(DISABLE_FC_NOTIFICATIONS);
@@ -62,6 +96,11 @@ public class MiscOptions extends SettingsPreferenceFragment implements OnPrefere
         mHfmDisableAds = (CheckBoxPreference) findPreference(HFM_DISABLE_ADS);
         mHfmDisableAds.setChecked((Settings.System.getInt(resolver,
                 Settings.System.HFM_DISABLE_ADS, 0) == 1));
+
+        mIncludedAppCircleBar = (AppMultiSelectListPreference) prefSet.findPreference(PREF_INCLUDE_APP_CIRCLE_BAR_KEY);
+        Set<String> includedApps = getIncludedApps();
+        if (includedApps != null) mIncludedAppCircleBar.setValues(includedApps);
+        mIncludedAppCircleBar.setOnPreferenceChangeListener(this);
 
     }
 
@@ -87,9 +126,36 @@ public class MiscOptions extends SettingsPreferenceFragment implements OnPrefere
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object value) {
-         return true;
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mIncludedAppCircleBar) {
+            storeIncludedApps((Set<String>) newValue);
+        } else {
+            return false;
+        }
+        return true;
     }
+
+    private Set<String> getIncludedApps() {
+        String included = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.WHITELIST_APP_CIRCLE_BAR);
+        if (TextUtils.isEmpty(included)) {
+            return null;
+        }
+        return new HashSet<String>(Arrays.asList(included.split("\\|")));
+    }
+
+    private void storeIncludedApps(Set<String> values) {
+        StringBuilder builder = new StringBuilder();
+        String delimiter = "";
+        for (String value : values) {
+            builder.append(delimiter);
+            builder.append(value);
+            delimiter = "|";		 
+		}
+        Settings.System.putString(getActivity().getContentResolver(),
+                Settings.System.WHITELIST_APP_CIRCLE_BAR, builder.toString());
+	}
 
     public static class DeviceAdminLockscreenReceiver extends DeviceAdminReceiver {}
 
