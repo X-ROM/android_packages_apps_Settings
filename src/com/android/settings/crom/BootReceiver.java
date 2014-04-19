61b65c5215a40a8ad5d865a33ec0f421cbfff4e9
aways <aways@c-rom.org>
2014-04-19 21:10:34 +0200

    Fix Build CREDITS CM

--- src/com/android/settings/crom/BootReceiver.java
+++ src/com/android/settings/crom/BootReceiver.java
@@ -22,6 +22,10 @@ import android.content.Intent;
 
 import com.android.settings.DisplaySettings;
 import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.hardware.DisplayColor;
+import com.android.settings.hardware.DisplayGamma;
+import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.location.LocationSettings;
 
 public class BootReceiver extends BroadcastReceiver {
@@ -31,8 +35,11 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context ctx, Intent intent) {
 
+        /* Restore the hardware tunable values */
+        DisplayColor.restore(ctx);
+        DisplayGamma.restore(ctx);
+        VibratorIntensity.restore(ctx);
         DisplaySettings.restore(ctx);
         LocationSettings.restore(ctx);
-        VibratorIntensity.restore(ctx);
     }
 }
