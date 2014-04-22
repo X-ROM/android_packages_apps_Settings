AICP/packages_apps_Settings

Ad blocker revisited
*Pull and parse hosts from web
*Uses following command line utils: wget, sort, cat, cp, echo, rm, mount, sed

PS1: Move to a new PreferenceScreen
PS2: Move HFM classes to /aicp
PS3: Strings, PreferenceScreen stuff
PS4: rebase
PS5: Strings, actual code
PS6: Rebased
PS7: Automatic applying oughta work
PS8: Fix previous, clean debug, add toast message
PS9: Rebased
PS10: Fix threading FC
PS11: Fix wonky toast messages, shorten timeout, add a pref check
PS12: Timeout tweak
PS13: Fix a bug in which isScriptFinished() would return an incorrect value
PS14: Fix a couple FCs, tweak failed toast string
PS15: Move sources  again, fix IntentReceiver FC
PS16: Pushed by accident
PS17: Rebased
PS18: Fix derpy, add a fancy schmancy progress dialog
PS19: Rebased
PS20: Move most methods to HfmHelpers, Use dialogs to prevent toast congestion,
make update pref dependant on main pref, use string resources,
consider old hosts when updating

Change-Id: I3d939a8ca7a09480d167e4d5a1f0927f40ade149

src/com/android/settings/aicp/hfm/CheckHosts.java
@@ -14,26 +14,36 @@
  * limitations under the License.
  */
 
-package com.android.settings.hfm;
+package com.android.settings.aicp.hfm;
 
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.preference.PreferenceManager;
+import android.os.IBinder;
 import android.util.Log;
 
-public class IntentReceiver extends BroadcastReceiver {
+public class CheckHosts extends Service {
 
-    private static final String TAG = "HFMBootReceiver";
+    private static final String TAG = "HFMCheckHosts";
 
-    public static final String ACTION_RUN_BOOTCOMPLETE = "android.intent.action.BOOT_COMPLETED";
+    public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    private Context mContext;
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "Started Receiver");
-        Intent serv = new Intent(context, CheckHosts.class);
-        serv.setAction(intent.getAction());
-        serv.putExtras(intent);
-        context.startService(serv);
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        mContext = this;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log.i(TAG, "Started check");
+        HfmHelpers.checkStatus(mContext);
     }
 }

src/com/android/settings/aicp/hfm/FetchHosts.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aicp.hfm;
+
+import java.io.IOException;
+
+import com.android.settings.aicp.hfm.HfmHelpers;
+
+public class FetchHosts {
+
+    public static String[] stringUrl = {
+        "http://hosts-file.net/.%5Cad_servers.txt",
+        "http://winhelp2002.mvps.org/hosts.txt",
+        "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
+        "http://adaway.org/hosts.txt"
+    };
+
+    public static int successfulSources;
+
+    public static void fetch() throws IOException {
+
+        String cmd = "mount -o rw,remount /system && echo \'foo\' > /etc/started.cfg && sed -i -e '$G' /etc/hosts.alt";
+
+        successfulSources = 0;
+
+        int i = 0;
+        while (i <= 3) {
+            if (HfmHelpers.isAvailable(stringUrl[i])) {
+                cmd = cmd + " && wget \"" + stringUrl[i] + "\" -O /etc/hosts" + i; //Get file
+                cmd = cmd + " && sed -i -e '$G' /etc/hosts" + i; //Put newline
+                successfulSources++;
+            }
+            i++;
+        }
+
+        cmd = cmd + " && cat /etc/hosts[0-9] /etc/hosts.alt > /etc/hosts.tmp" //Merge old & new hosts
+                  + " && sort -u /etc/hosts.tmp -o /etc/hosts.tmp" //Remove duplicate lines
+                  + " && sed -i '/^[@#]/ d' /etc/hosts.tmp" //Remove commented lines
+                  + " && sed -i '/^$/d' /etc/hosts.tmp" //Remove blank lines
+                  + " && cp -f /etc/hosts.tmp /etc/hosts.alt"
+                  + " && rm -f /etc/hosts[0-9] /etc/hosts.tmp /etc/started.cfg" //Clean up
+                  + " && mount -o ro,remount /system";
+
+       HfmHelpers.RunAsRoot(cmd);
+    }
+}

src/com/android/settings/aicp/hfm/HfmSettings.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aicp.hfm;
+
+import java.io.IOException;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.View;
+
+import com.android.settings.aicp.hfm.FetchHosts;
+import com.android.settings.aicp.hfm.HfmHelpers;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class HfmSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "HfmSettings";
+    private static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    public static ProgressDialog pd;
+
+    ConnectivityManager connMgr;
+
+    public static CheckBoxPreference mHfmDisableAds;
+    Preference mHfmUpdateHosts;
+
+    Context context;
+
+    public static Resources res;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = getActivity();
+        res = getResources();
+        pd = new ProgressDialog(context);
+        pd.setCancelable(false);
+        pd.setMessage(res.getString(R.string.hfm_dialog_wait));
+        connMgr = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+
+        addPreferencesFromResource(R.xml.hfm_settings);
+        ContentResolver resolver = context.getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mHfmDisableAds = (CheckBoxPreference) findPreference(HFM_DISABLE_ADS);
+        mHfmDisableAds.setChecked((Settings.System.getInt(resolver,
+            Settings.System.HFM_DISABLE_ADS, 0) == 1));
+        mHfmUpdateHosts = prefScreen.findPreference("hfm_update_hosts");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        if  (preference == mHfmDisableAds) {
+            boolean checked = ((CheckBoxPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.HFM_DISABLE_ADS, checked ? 1:0);
+            HfmHelpers.checkStatus(getActivity());
+        } else if (preference == mHfmUpdateHosts) {
+            try {
+                HfmHelpers.checkConnectivity(context, connMgr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        return true;
+    }
+}

src/com/android/settings/aicp/hfm/IntentReceiver.java
@@ -14,36 +14,26 @@
  * limitations under the License.
  */
 
-package com.android.settings.hfm;
+package com.android.settings.aicp.hfm;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class CheckHosts extends Service {
+public class IntentReceiver extends BroadcastReceiver {
 
-    private static final String TAG = "HFMCheckHosts";
+    private static final String TAG = "HFMBootReceiver";
 
-    public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
-
-    private Context mContext;
+    public static final String ACTION_RUN_BOOTCOMPLETE = "android.intent.action.BOOT_COMPLETED";
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    public void onCreate() {
-        mContext = this;
-    }
-
-    @Override
-    public void onStart(Intent intent, int startId) {
-        Log.i(TAG, "Started check");
-        HfmHelpers.checkStatus(mContext);
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Started Receiver");
+        Intent serv = new Intent(context, CheckHosts.class);
+        serv.setAction(intent.getAction());
+        serv.putExtras(intent);
+        context.startService(serv);
     }
 }

X-ROM/android_packages_apps_Settings

src/com/android/settings/crom/hfm/CheckHosts.java
@@ -14,26 +14,36 @@
  * limitations under the License.
  */
 
-package com.android.settings.hfm;
+package com.android.settings.crom.hfm;
 
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.preference.PreferenceManager;
+import android.os.IBinder;
 import android.util.Log;
 
-public class IntentReceiver extends BroadcastReceiver {
+public class CheckHosts extends Service {
 
-    private static final String TAG = "HFMBootReceiver";
+    private static final String TAG = "HFMCheckHosts";
 
-    public static final String ACTION_RUN_BOOTCOMPLETE = "android.intent.action.BOOT_COMPLETED";
+    public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    private Context mContext;
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "Started Receiver");
-        Intent serv = new Intent(context, CheckHosts.class);
-        serv.setAction(intent.getAction());
-        serv.putExtras(intent);
-        context.startService(serv);
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        mContext = this;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log.i(TAG, "Started check");
+        HfmHelpers.checkStatus(mContext);
     }
 }

src/com/android/settings/crom/hfm/FetchHosts.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.crom.hfm;
+
+import java.io.IOException;
+
+import com.android.settings.crom.hfm.HfmHelpers;
+
+public class FetchHosts {
+
+    public static String[] stringUrl = {
+        "http://hosts-file.net/.%5Cad_servers.txt",
+        "http://winhelp2002.mvps.org/hosts.txt",
+        "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
+        "http://adaway.org/hosts.txt"
+    };
+
+    public static int successfulSources;
+
+    public static void fetch() throws IOException {
+
+        String cmd = "mount -o rw,remount /system && echo \'foo\' > /etc/started.cfg && sed -i -e '$G' /etc/hosts.alt";
+
+        successfulSources = 0;
+
+        int i = 0;
+        while (i <= 3) {
+            if (HfmHelpers.isAvailable(stringUrl[i])) {
+                cmd = cmd + " && wget \"" + stringUrl[i] + "\" -O /etc/hosts" + i; //Get file
+                cmd = cmd + " && sed -i -e '$G' /etc/hosts" + i; //Put newline
+                successfulSources++;
+            }
+            i++;
+        }
+
+        cmd = cmd + " && cat /etc/hosts[0-9] /etc/hosts.alt > /etc/hosts.tmp" //Merge old & new hosts
+                  + " && sort -u /etc/hosts.tmp -o /etc/hosts.tmp" //Remove duplicate lines
+                  + " && sed -i '/^[@#]/ d' /etc/hosts.tmp" //Remove commented lines
+                  + " && sed -i '/^$/d' /etc/hosts.tmp" //Remove blank lines
+                  + " && cp -f /etc/hosts.tmp /etc/hosts.alt"
+                  + " && rm -f /etc/hosts[0-9] /etc/hosts.tmp /etc/started.cfg" //Clean up
+                  + " && mount -o ro,remount /system";
+
+       HfmHelpers.RunAsRoot(cmd);
+    }
+}

src/com/android/settings/crom/hfm/HfmSettings.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.crom.hfm;
+
+import java.io.IOException;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.View;
+
+import com.android.settings.crom.hfm.FetchHosts;
+import com.android.settings.crom.hfm.HfmHelpers;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class HfmSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "HfmSettings";
+    private static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    public static ProgressDialog pd;
+
+    ConnectivityManager connMgr;
+
+    public static CheckBoxPreference mHfmDisableAds;
+    Preference mHfmUpdateHosts;
+
+    Context context;
+
+    public static Resources res;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = getActivity();
+        res = getResources();
+        pd = new ProgressDialog(context);
+        pd.setCancelable(false);
+        pd.setMessage(res.getString(R.string.hfm_dialog_wait));
+        connMgr = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+
+        addPreferencesFromResource(R.xml.hfm_settings);
+        ContentResolver resolver = context.getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mHfmDisableAds = (CheckBoxPreference) findPreference(HFM_DISABLE_ADS);
+        mHfmDisableAds.setChecked((Settings.System.getInt(resolver,
+            Settings.System.HFM_DISABLE_ADS, 0) == 1));
+        mHfmUpdateHosts = prefScreen.findPreference("hfm_update_hosts");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        if  (preference == mHfmDisableAds) {
+            boolean checked = ((CheckBoxPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.HFM_DISABLE_ADS, checked ? 1:0);
+            HfmHelpers.checkStatus(getActivity());
+        } else if (preference == mHfmUpdateHosts) {
+            try {
+                HfmHelpers.checkConnectivity(context, connMgr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        return true;
+    }
+}

src/com/android/settings/crom/hfm/IntentReceiver.java
@@ -14,36 +14,26 @@
  * limitations under the License.
  */
 
-package com.android.settings.hfm;
+package com.android.settings.crom.hfm;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class CheckHosts extends Service {
+public class IntentReceiver extends BroadcastReceiver {
 
-    private static final String TAG = "HFMCheckHosts";
+    private static final String TAG = "HFMBootReceiver";
 
-    public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
-
-    private Context mContext;
+    public static final String ACTION_RUN_BOOTCOMPLETE = "android.intent.action.BOOT_COMPLETED";
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    public void onCreate() {
-        mContext = this;
-    }
-
-    @Override
-    public void onStart(Intent intent, int startId) {
-        Log.i(TAG, "Started check");
-        HfmHelpers.checkStatus(mContext);
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Started Receiver");
+        Intent serv = new Intent(context, CheckHosts.class);
+        serv.setAction(intent.getAction());
+        serv.putExtras(intent);
+        context.startService(serv);
     }
 }