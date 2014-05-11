85cc1e52a24db0b87c9790bfbb34fe973aa675ac
aways <aways@c-rom.org>
2014-05-11 18:13:35 +0200

    Revert "Update for Performance settings"
    This reverts commit 668853b59d718df7eb86a34cdcad405d5634745c.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -658,10 +658,6 @@ public class Settings extends PreferenceActivity
                 if (!showDev) {
                     target.remove(i);
                 }
-            } else if (id == R.id.performance_settings) {
-                if (!showDev) {
-                    target.remove(i);
-                }
             } else if (id == R.id.account_add) {
                 if (um.hasUserRestriction(UserManager.DISALLOW_MODIFY_ACCOUNTS)) {
                     target.remove(i);
